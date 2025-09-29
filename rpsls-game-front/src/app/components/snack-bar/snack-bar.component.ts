98ebc3ad8d6647a935833cf21dfde8da851dc956
Kleber <kleberandrade1998@hotmail.com>
2025-09-29 20:08:53 -0300

    update

--- rpsls-game-front/src/app/components/snack-bar/snack-bar.component.ts
+++ rpsls-game-front/src/app/components/snack-bar/snack-bar.component.ts
@@ -43,7 +43,6 @@ export class SnackBarComponent implements OnInit {
         this.icon.set('');
         this.color.set('black');
     }
-    console.log('snackBarRef', this.data())
   }
 
   getIcon(status: string) {
