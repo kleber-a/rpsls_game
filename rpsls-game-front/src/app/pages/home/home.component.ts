d9e18db89218b749f200c1a53f41a4e773835346
Kleber <kleberandrade1998@hotmail.com>
2025-09-29 21:54:12 -0300

    update

--- rpsls-game-front/src/app/pages/home/home.component.ts
+++ rpsls-game-front/src/app/pages/home/home.component.ts
@@ -9,6 +9,7 @@ import {MatButtonModule} from '@angular/material/button';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { DialogRoomComponent } from '../../components/dialog-room/dialog-room.component';
 import { ApiService } from '../../api.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -16,10 +17,15 @@ import { ApiService } from '../../api.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   #dialog = inject(MatDialog);
   #service = inject(ApiService);
+  #title = inject(Title);
+
+  ngOnInit(): void {
+      this.#title.setTitle('RPSLS - Pedra, Papel, Tesoura, Lagarto e Spock')
+  }
 
   openDialog() {
     const dialogRef = this.#dialog.open(DialogRoomComponent, {
